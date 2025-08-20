60fc58d802388ccf971ba780f116aaf9b974b667
Emma Hernandez <emmacecilia02@gmail.com>
2025-08-20 19:30:38 -0400

    app py updated for flask

--- app.py
+++ app.py
@@ -229,4 +229,5 @@ def get_calendar_events():
     return jsonify(formatted_events)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
