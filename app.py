3d3e34c6ad74e0f207f3bb3e47c3f7fb5eecc644
Emma Hernandez <emmacecilia02@gmail.com>
2025-08-20 23:09:38 -0400

    who kmnows lol

--- app.py
+++ app.py
@@ -163,21 +163,21 @@ def delete_kanban_task():
 
 # --- Guest Address Endpoints ---
 # UPDATED: This endpoint now joins the guest_list and parties table to get all address info
+# In your app.py file, replace the existing get_guests endpoint
+# In your app.py file, replace the existing get_guests endpoint
 @app.route('/api/guests', methods=['GET'])
 def get_guests():
-    # Correct table name is guest_list, not guests
     query = sql.SQL("""
         SELECT
-            g.guest_id AS id,
-            p.party_name AS partyName,
-            p.address_collection_assigned_to_id AS assignedTo,
-            p.address_street AS street,
-            p.address_street2 AS street2,
-            p.address_city AS city,
-            p.address_state AS state,
-            p.address_zip AS zip
-        FROM public.guest_list AS g
-        JOIN public.parties AS p ON g.party_id = p.party_id;
+            party_id AS id,
+            party_name AS partyName,
+            address_collection_assigned_to_name AS assignedTo,
+            address_street AS street,
+            address_street2 AS street2,
+            address_city AS city,
+            address_state AS state,
+            address_zip AS zip
+        FROM public.parties;
     """)
     guests, error = get_data_from_query(query)
     if error:
